7ac6c0cb6c9c92114bb19f1ba85c8529765b9d04
siyu hang <hangs988@newschool.edu>
2018-10-23 20:34:37 -0400

    week7hw

--- week7hw/week7.js
+++ week7hw/week7.js
@@ -101,7 +101,7 @@ for (var i = 0; i < 10; i = i + 1) {
     for (var j = 0; j < 10; j = j + 1) {
         console.log("Row " + i + ", Column " + j);
 
-        if (j == 3) {
+        if (j % 2 == 0) {
             console.log("   pink");
             document.write("<div class='box pink'></div>");
         } else {
